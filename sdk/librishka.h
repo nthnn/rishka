cd5350e1e2dcd0fb6e7ce5ea1fd787f156238ac1
nthnn <nathanneisip@gmail.com>
2024-02-11 06:11:26 +0800

    librishka library header.

---
+++ sdk/librishka.h
@@ -0,0 +1,10 @@
+#ifndef LIBRISHKA_H
+#define LIBRISHKA_H
+
+void io_prints(char* arg0);
+void io_printn(long arg0);
+char io_readch();
+
+void* sys_memset(void* arg0, int arg1, unsigned int arg2);
+
+#endif
