5b224a811678947a599dd8b578d390d3c766f219
nthnn <nathanneisip@gmail.com>
2024-02-14 00:19:54 +0800

    Liquidated librishka.h header.

--- sdk/librishka.h
+++ sdk/librishka.h
@@ -15,44 +15,12 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #ifndef LIBRISHKA_H
 #define LIBRISHKA_H
 
-typedef char                rune;
-typedef char*               string;
-
-typedef signed char         i8;
-typedef signed short int    i16;
-typedef signed int          i32;
-typedef signed long int     i64;
-
-typedef unsigned char       u8;
-typedef unsigned short int  u16;
-typedef unsigned int        u32;
-typedef unsigned long int   u64;
-
-typedef u32                 usize;
-typedef void*               any;
-
-#define F(str) ((char*) str)
-
-class IO {
-public:
-    static void prints(const string text);
-    static void printn(i64 number);
-    static void printd(double number);
-    static rune readch();
-};
-
-class Sys {
-public:
-    static void exit(i32 code);
-};
-
-class Memory {
-public:
-    static any set(any dest, i32 c, usize n);
-};
+#include <librishka/io.h>
+#include <librishka/memory.h>
+#include <librishka/sys.h>
+#include <librishka/types.h>
 
 #endif
