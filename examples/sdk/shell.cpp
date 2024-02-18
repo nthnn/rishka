7558741d322e32b2c9dc95fa2a72bfa61df90ea0
Nathanne Isip <nathanneisip@gmail.com>
2024-02-18 16:54:51 +0800

    Sys::shellexec() example on SDK.

---
+++ examples/sdk/shell.cpp
@@ -0,0 +1,28 @@
+/* 
+ * This file is part of the Rishka SDK distribution (https://github.com/rishka-esp32/rishka-sdk).
+ * Copyright (c) 2024 Nathanne Isip.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <librishka.h>
+
+int main() {
+    int exitcode = Sys::shellexec(F("/hello.bin"), 0, (char**) nil);
+
+    IO::prints(F("Exit code: "));
+    IO::printn(exitcode);
+    IO::prints(F("\n"));
+
+    return 0;
+}
