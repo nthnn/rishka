e4469a5426f905fd03cd0c25920ac585698dad6f
Nathanne Isip <nathanneisip@gmail.com>
2024-02-28 00:59:50 +0800

    librishka/fs.h system call implementations.

--- sdk/librishka_impl.cpp
+++ sdk/librishka_impl.cpp
@@ -191,15 +191,15 @@ static inline string get_rt_string(u32 len) {
     return str;
 }
 
-void IO::prints(const string text) {
+void IO::print(const string text) {
     rishka_sc_1(RISHKA_SC_IO_PRINTS, (i64) text);
 }
 
-void IO::printn(i64 number) {
+void IO::print(i64 number) {
     rishka_sc_1(RISHKA_SC_IO_PRINTN, (i64) number);
 }
 
-void IO::printd(double number) {
+void IO::print(double number) {
     rishka_sc_1(RISHKA_SC_IO_PRINTD, double_to_long(number));
 }
 
@@ -318,4 +318,72 @@ void Int::attach(u8 pin, void (*callback)(void), int_mode_t mode) {
 
 void Int::detach(u8 pin) {
     rishka_sc_1(RISHKA_SC_INT_DETACH, (i64) pin);
+}
+
+File::File(i32 _handle) {
+    this->handle = _handle;
+}
+
+File File::open(string file, string mode) {
+    return File((i32) rishka_sc_2(RISHKA_SC_FS_OPEN, (i64) file, (i64) mode));
+}
+
+bool File::is_file() {
+    return (bool) rishka_sc_1(RISHKA_SC_FS_ISFILE, (i64) this->handle);
+}
+
+bool File::is_dir() {
+    return (bool) rishka_sc_1(RISHKA_SC_FS_ISDIR, (i64) this->handle);
+}
+
+i32 File::available() {
+    return (i32) rishka_sc_1(RISHKA_SC_FS_AVAILABLE, (i64) this->handle);
+}
+
+i32 File::peek() {
+    return (i32) rishka_sc_1(RISHKA_SC_FS_PEEK, (i64) this->handle);
+}
+
+bool File::seek(u32 pos) {
+    return (bool) rishka_sc_2(RISHKA_SC_FS_SEEK, (i64) this->handle, (i64) pos);
+}
+
+usize File::size() {
+    return (usize) rishka_sc_1(RISHKA_SC_FS_SIZE, (i64) this->handle);
+}
+
+i32 File::read() {
+    return (i32) rishka_sc_1(RISHKA_SC_FS_READ, (i64) this->handle);
+}
+
+void File::write(string data) {
+    rishka_sc_2(RISHKA_SC_FS_WRITE, (i64) this->handle, (i64) data);
+}
+
+File File::next(string mode) {
+    return File(rishka_sc_2(RISHKA_SC_FS_NEXT, (i64) this->handle, (i64) mode));
+}
+
+void File::flush() {
+    rishka_sc_1(RISHKA_SC_FS_FLUSH, (i64) this->handle);
+}
+
+void File::close() {
+    rishka_sc_1(RISHKA_SC_FS_CLOSE, (i64) this->handle);
+}
+
+bool mkdir(const char* path) {
+    return (bool) rishka_sc_1(RISHKA_SC_FS_MKDIR, (i64) path);
+}
+
+bool rmdir(const char* path) {
+    return (bool) rishka_sc_1(RISHKA_SC_FS_RMDIR, (i64) path);
+}
+
+bool remove(const char* path) {
+    return (bool) rishka_sc_1(RISHKA_SC_FS_DELETE, (i64) path);
+}
+
+bool exists(const char* path) {
+    return (bool) rishka_sc_1(RISHKA_SC_FS_EXISTS, (i64) path);
 }
