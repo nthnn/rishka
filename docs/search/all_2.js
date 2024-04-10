357086c90b1bcf98323560038ddf87acad2ca6ef
nthnn <nathanneisip@gmail.com>
2024-04-11 05:30:18 +0800

    Updated SDK documentations.

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['calloc_0',['calloc',['../d2/d01/class_memory.html#af8365b5845b980ce2ccc5b77369bc4a6',1,'Memory']]],
-  ['close_1',['close',['../d1/d31/class_file.html#a83cbce54d6c3b8c2f417b51f6b3f488c',1,'File']]],
-  ['count_2',['count',['../dc/dc6/class_args.html#a3735445f3af219cd1104000e50484a0d',1,'Args']]]
+  ['changedirectory_1',['changeDirectory',['../d0/d04/class_sys.html#abc14702959528aa2c6abb0ac3771a68b',1,'Sys']]],
+  ['close_2',['close',['../d1/d31/class_file.html#a83cbce54d6c3b8c2f417b51f6b3f488c',1,'File']]],
+  ['count_3',['count',['../dc/dc6/class_args.html#a3735445f3af219cd1104000e50484a0d',1,'Args']]]
 ];
